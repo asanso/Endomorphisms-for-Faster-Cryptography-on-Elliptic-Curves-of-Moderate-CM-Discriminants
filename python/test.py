5867145718a9b4018244e8382daaa19d859d7185
Antonio Sanso <antonio.sanso@gmail.com>
2024-11-28 11:24:09 +0100

    classes

--- python/test.py
+++ python/test.py
@@ -8,6 +8,16 @@ r = 1910157204347957325700187962477453761504460135807772883063193
 E = WeierstrassCurve(p, a, b, r, 1)
 
 P = E.random_point()
+Q = E.random_point()
+
+# add
+R = P.add(Q)
+assert R.on_curve()
+assert R == Q.add(P)
+
+# double
+R = P.double()
+assert R.on_curve()
 
 # scalar mul
 n = 5
