f5a570cfacd193255eace3a87ef628ba775f7b03
Antonio Sanso <antonio.sanso@gmail.com>
2024-11-28 21:02:09 +0100

    edit

--- python/bench.py
+++ python/bench.py
@@ -15,11 +15,13 @@ n = random.randint(0,r)
 print(n)
 
 t = cputime()
-P.scalar_mul(n)
+for i in range(1000):
+    P.scalar_mul(n)
 t = cputime(t)
 print("scalar multiplication:\t\t\t{:5.3f}ms".format(t))
 
 t2 = cputime()
-P.fast_scalar_mul(n)
+for i in range(1000):
+    P.fast_scalar_mul(n)
 t2 = cputime(t2)
 print('GLV:\t\t{:5.3f}ms ({:2.0f}% faster)'.format(t2, 100*(t-t2)/t2))
