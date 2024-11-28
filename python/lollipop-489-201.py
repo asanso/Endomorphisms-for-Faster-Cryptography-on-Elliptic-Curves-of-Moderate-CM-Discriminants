3292cb6a9f9af5ac1d79296e9355e84b6aa4d2a2
Antonio Sanso <antonio.sanso@gmail.com>
2024-11-28 13:01:24 +0100

    classes

--- python/lollipop-489-201.py
+++ python/lollipop-489-201.py
@@ -8,5 +8,22 @@ from curve import PointWeierstrass
 
 class  Lollipop489201(WeierstrassCurve):
 
-       def __init__(self, p, a, b, r, cofactor):
+       def __init__(self, p, a, b, r, cofactor,L):
             super().__init__(p, a,b, r, cofactor)
+            self.D = -547
+            self.L = L
+            self.cofactor = cofactor
+            self.r = r
+            M = Matrix([[-L,1], [r,0]])
+            self.N = M.LLL()
+            self.N_inv = self.N**-1
+
+class Lollipop489201(PointWeierstrass):
+      
+        def fast_scalar_mul(self, n):
+            psiP = self.psi()
+            beta = vector([n,0]) * self.curve.N_inv
+            b = vector([int(beta[0]), int(beta[1])]) * self.curve.N
+            k1 = n-b[0]
+            k2 = -b[1]
+            return self.multi_scalar_mul(k1, psiP, k2)
