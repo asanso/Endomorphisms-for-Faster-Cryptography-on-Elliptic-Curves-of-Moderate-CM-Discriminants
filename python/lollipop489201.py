5afed54a4b72634f245a65f9d7386d1c14a4dfbd
Antonio Sanso <antonio.sanso@gmail.com>
2024-12-04 09:30:09 +0100

    clean

--- python/lollipop489201.py
+++ python/lollipop489201.py
@@ -1,6 +1,4 @@
-import sage.all
 from sage.all import FractionField
-from sage.rings.integer_ring import Z as ZZ
 from sage.matrix.constructor import Matrix
 from sage.modules.free_module_element import free_module_element as vector
 from curve import WeierstrassCurve
