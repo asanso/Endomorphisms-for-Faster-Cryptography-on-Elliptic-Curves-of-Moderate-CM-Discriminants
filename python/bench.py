a8fc44f816c4d1abc7fb4c9b1363e1394ccb5d99
Antonio Sanso <antonio.sanso@gmail.com>
2024-12-30 12:19:20 +0100

    gc256c

--- python/bench.py
+++ python/bench.py
@@ -1,7 +1,8 @@
 import random
 from sage.misc.misc import cputime
-# Bandersnatch curve
 from lollipop489201 import Lollipop489201
+from gc256c import GC256C
+# Lollipop489201
 
 p = 1910157204347957325700187962480217512925138482090399484362397
 a=73275333332267847499581501376863252276520692179021512625126;
@@ -13,6 +14,46 @@ E = Lollipop489201(p, a, b, r, 1, eigen)
 P = E.random_point()
 n = random.randint(0,r)
 l = 101
+
+t = cputime()
+for i in range(1000):
+    P.scalar_mul(2**l)
+t = cputime(t)
+print("scalar multiplication:\t\t\t{:5.3f}ms".format(t))
+
+t2 = cputime()
+for i in range(1000):
+    P.psi()
+t2 = cputime(t2)
+print('endomorphism:\t\t{:5.3f}ms ({:2.0f}% faster)'.format(t2, 100*(t-t2)/t2))
+
+print()
+t = cputime()
+for i in range(1000):
+    P.scalar_mul(n)
+t = cputime(t)
+print("scalar multiplication:\t\t\t{:5.3f}ms".format(t))
+
+t2 = cputime()
+for i in range(1000):
+    P.fast_scalar_mul(n)
+t2 = cputime(t2)
+print('GLV:\t\t{:5.3f}ms ({:2.0f}% faster)'.format(t2, 100*(t-t2)/t2))
+
+#GC256C
+
+p = 2**255 + 3225
+a =  p -3
+b = 28091019353058090096996979000309560759124368558014865957655842872397301267595
+r = 57896044618658097711785492504343953927102133160255826820068844496087732066703
+eigen = 25745384436187293773257686313148572361251961181400080445531962069440031481800
+
+E = GC256C(p, a, b, r, 1, eigen)
+E = Lollipop489201(p, a, b, r, 1, eigen)
+P = E.random_point()
+n = random.randint(0,r)
+l = 128
+
 
 t = cputime()
 for i in range(1000):
